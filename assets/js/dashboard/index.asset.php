23f6f7da891ea075ccd5d35a87cd54816527703e
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2026-04-05 17:08:55 +0530

    Update build files

--- assets/js/dashboard/index.asset.php
+++ assets/js/dashboard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '86fccfba1f9fd5f2e688');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f28c87d95e43e07ce9b4');
